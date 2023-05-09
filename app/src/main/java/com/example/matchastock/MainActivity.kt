ffc692ba2eb451497f6f4d5bbbd1fdb9201904fc
Blanca <blanca.rosales13620@est.uca.edu.ni>
2023-05-08 22:37:18 -0600

    inicio de diseño de UI

--- app/src/main/java/com/example/matchastock/MainActivity.kt
+++ app/src/main/java/com/example/matchastock/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.matchastock
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import com.example.matchastock.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+    private lateinit var navView: NavigationView
+
+    private  lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
     }
 }
