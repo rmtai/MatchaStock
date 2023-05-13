f6cf5055ef5e1a30da85fc36dbaeaf826ab8ca24
ashly31 <105886487+ashly31@users.noreply.github.com>
2023-05-12 21:23:28 -0800

    Add files via upload

---
+++ MatchaStock/Usuario/mostrar.php
@@ -0,0 +1,20 @@
+<?php
+if($_SERVER["REQUEST_METHOD"]=="GET"){
+    require_once 'conexion.php';
+    $my_query = "select * from usuario ";
+    $result = $mysql->query($my_query);
+    if($mysql->affected_rows>0){
+        $json = "{\"data\": [";
+            while($row = $result->fetch_assoc()){
+                $json = $json.json_encode($row);
+                $json=$json.",";
+            }
+            $json=substr(trim($json),0,-1);
+            $json = $json."]}";
+    }
+    echo $json;
+    $result->close();
+    $mysql->close();
+}
+
+?>
---
+++ MatchaStock/matchastockdb.sql
@@ -0,0 +1,118 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 12-05-2023 a las 05:24:33
+-- Versión del servidor: 10.4.24-MariaDB
+-- Versión de PHP: 8.0.19
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `matchastockdb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `producto`
+--
+
+CREATE TABLE `producto` (
+  `idItem` int(11) NOT NULL,
+  `nombreProd` varchar(25) NOT NULL,
+  `descripcionProd` varchar(50) NOT NULL,
+  `cantidadProd` int(100) NOT NULL,
+  `estado` int(11) NOT NULL,
+  `idUser` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `producto`
+--
+
+INSERT INTO `producto` (`idItem`, `nombreProd`, `descripcionProd`, `cantidadProd`, `estado`, `idUser`) VALUES
+(2, 'Te verde', 'Te verde medicinal y relajante', 20, 1, 2),
+(3, 'Cafe negro', 'Un buen cafe negro para los amantes', 76, 2, 4),
+(4, 'jhvhgc', 'hbxhjjb', 76876, 3, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `idUser` int(11) NOT NULL,
+  `nombre` text NOT NULL,
+  `apellido` text NOT NULL,
+  `username` varchar(15) NOT NULL,
+  `passwordUser` varchar(10) NOT NULL,
+  `email` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `usuario`
+--
+
+INSERT INTO `usuario` (`idUser`, `nombre`, `apellido`, `username`, `passwordUser`, `email`) VALUES
+(2, 'Blanca Tais', 'Rosales Martinez', 'BTais', '1234', 'blancatais09@gmail.com'),
+(4, 'Mabel', 'Garcia', 'gmabs', '1234', 'mabelgarcia13@gmail.com');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `producto`
+--
+ALTER TABLE `producto`
+  ADD PRIMARY KEY (`idItem`),
+  ADD KEY `idUser` (`idUser`);
+
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`idUser`),
+  ADD KEY `idUser` (`idUser`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `producto`
+--
+ALTER TABLE `producto`
+  MODIFY `idItem` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `idUser` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `producto`
+--
+ALTER TABLE `producto`
+  ADD CONSTRAINT `usuario_idUser` FOREIGN KEY (`idUser`) REFERENCES `usuario` (`idUser`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
