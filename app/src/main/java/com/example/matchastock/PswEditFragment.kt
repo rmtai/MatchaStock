8778c32e4263febc2938e4435e7baf5d61f6432b
ashly31 <ashly.castillo13913@est.uca.edu.ni>
2023-05-12 21:07:21 -0500

    Navegación entre pantallas

--- app/src/main/java/com/example/matchastock/PswEditFragment.kt
+++ app/src/main/java/com/example/matchastock/PswEditFragment.kt
@@ -5,28 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.matchastock.databinding.FragmentPswEditBinding
+import com.example.matchastock.databinding.FragmentUserEditBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [PswEditFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class PswEditFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var binding : FragmentPswEditBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
     override fun onCreateView(
@@ -34,26 +22,18 @@ class PswEditFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_psw_edit, container, false)
+        binding = FragmentPswEditBinding.inflate(inflater, container, false)
+
+        binding.btnActPsw.setOnClickListener{
+            findNavController().navigate(R.id.action_pswEditFragment_to_userInfoFragment2)
+        }
+
+        binding.btnCancelPsw.setOnClickListener{
+            findNavController().navigate(R.id.action_pswEditFragment_to_userInfoFragment2)
+        }
+
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment PswEditFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            PswEditFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
+
 }
--- app/src/main/java/com/example/matchastock/UserInfoFragment.kt
+++ app/src/main/java/com/example/matchastock/UserInfoFragment.kt
@@ -5,28 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.matchastock.databinding.FragmentSignUpBinding
+import com.example.matchastock.databinding.FragmentUserInfoBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [UserInfoFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class UserInfoFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    private lateinit var binding : FragmentUserInfoBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
     override fun onCreateView(
@@ -34,26 +23,26 @@ class UserInfoFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_user_info, container, false)
+        binding = FragmentUserInfoBinding.inflate(inflater, container, false)
+
+        binding.cvEditarPerfil.setOnClickListener{
+            findNavController().navigate(R.id.action_userInfoFragment_to_userEditFragment)
+        }
+
+        binding.cvCambiarPsw.setOnClickListener{
+            findNavController().navigate(R.id.action_userInfoFragment_to_pswEditFragment)
+        }
+
+        binding.cvCerrarSesion.setOnClickListener{
+            findNavController().navigate(R.id.action_userInfoFragment_to_loginFragment2)
+        }
+
+        binding.cvEliminarCuenta.setOnClickListener{
+            findNavController().navigate(R.id.action_userInfoFragment_to_signUpFragment)
+        }
+
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment UserInfoFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            UserInfoFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
+
 }
