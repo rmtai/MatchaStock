e99d59e671848731cdd88ce7b8785c4e35bc040c
Roire <roirevh@gmail.com>
2023-05-26 23:23:08 -0600

    Registro implementado

--- app/src/main/java/com/example/matchastock/SignUpFragment.kt
+++ app/src/main/java/com/example/matchastock/SignUpFragment.kt
@@ -5,19 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
+import com.example.matchastock.Controllers.UsuarioController
+import com.example.matchastock.Entities.User
 import com.example.matchastock.databinding.FragmentSignUpBinding
 
 
-class SignUpFragment : Fragment() {
+class SignUpFragment : Fragment(),UsuarioController.OnUsuarioRegistradoListener {
 
     private lateinit var  binding : FragmentSignUpBinding
+    private lateinit var usuarioController:UsuarioController
 
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,11 +25,67 @@ class SignUpFragment : Fragment() {
         // Inflate the layout for this fragment
         binding = FragmentSignUpBinding.inflate(inflater, container, false)
 
+        return binding.root
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
         binding.tvLogin.setOnClickListener{
             findNavController().navigate(R.id.action_signUpFragment_to_loginFragment)
         }
+        binding.btnRegister.setOnClickListener {
+            val nombre = binding.etName.text.toString()
+            val apellido = binding.etApellido.text.toString()
+            val username = binding.etUsername.text.toString()
+            val passwordUser = binding.etPsw.text.toString()
+            val email = binding.etEmail.text.toString()
 
-        return binding.root
+            if ( nombre.isEmpty() || apellido.isEmpty() || username.isEmpty()|| passwordUser.isEmpty() || email.isEmpty()) {
+                // Mostrar mensaje de error indicando que todos los campos deben estar completos.
+                Toast.makeText(
+                    requireContext(),
+                    "Todos los campos son requeridos",
+                    Toast.LENGTH_SHORT
+                ).show()
+                return@setOnClickListener
+            }
+
+            if (!android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                // Mostrar mensaje de error indicando que el formato del correo electrónico es inválido.
+                Toast.makeText(
+                    requireContext(),
+                    "Formato de correo electrónico inválido",
+                    Toast.LENGTH_SHORT
+                ).show()
+                return@setOnClickListener
+            }
+            val user = User(0, nombre, apellido, username, passwordUser, email)
+            register(user)
+        }
+        }
+
+    private fun register(usuario: User) {
+        usuarioController.agregarUsuario(usuario, this)
+        findNavController().navigate(R.id.action_signUpFragment_to_loginFragment)
+
+    }
+
+    override fun onUsuarioRegistradoExitosamente() {
+        val mensaje="El usuario se ha registrado correctamente."
+
+        requireActivity().runOnUiThread {
+            Toast.makeText(context,mensaje,Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onErrorRegistro(mensaje: String) {
+        val mensaje="Ocurrio un problema al registrar el usuario."
+
+        requireActivity().runOnUiThread {
+            Toast.makeText(context,mensaje,Toast.LENGTH_SHORT).show()
+        }
     }
 }
+
+
 
