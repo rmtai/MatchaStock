7dcd7cbed19f565f0a84dc5c86f8db9d78c08193
Blanca <blanca.rosales13620@est.uca.edu.ni>
2023-05-29 16:22:25 -0600

    cambios en el script login y dependencias

--- MatchaStock/Usuario/login.php
+++ MatchaStock/Usuario/login.php
@@ -1,34 +1,49 @@
 <?php
+session_start();
 include_once 'conexion.php';
 
-if (isset($_POST['username'])) {
+if (isset($_POST['username']) && isset($_POST['passwordUser'])) {
     $username = $_POST['username'];
-}
-if (isset($_POST['passwordUser'])) {
     $passwordUser = $_POST['passwordUser'];
-}
-$sentencia = $mysql->prepare("SELECT * FROM usuario WHERE username =? AND passwordUser=?");
-$sentencia->bind_param('ss', $username, $passwordUser);
-$sentencia->execute();
 
-$resultado = $sentencia->get_result();
-if ($row = $resultado->fetch_assoc()) {
+    $sentencia = $mysql->prepare("SELECT * FROM usuario WHERE username = ? AND passwordUser = ?");
+    $sentencia->bind_param('ss', $username, $passwordUser);
+    $sentencia->execute();
 
-    $response = array(
-        'success' => true, // Indica que el inicio de sesión fue exitoso
-        'message' => 'Inicio de sesión exitoso', // Mensaje adicional, puede personalizarse
-        'data' => $row // Puedes incluir cualquier otro dato que necesites en la respuesta
-    );
+    $resultado = $sentencia->get_result();
+    if ($row = $resultado->fetch_assoc()) {
+        // Verificar la existencia de los campos requeridos
+        if (isset($row['nombre']) && isset($row['apellido']) && isset($row['username']) && isset($row['passwordUser']) && isset($row['email'])) {
+            $response = array(
+                'success' => true,
+                'message' => 'Inicio de sesión exitoso',
+                'data' => $row
+            );
+
+            $_SESSION['username'] = $username;
+        } else {
+            $response = array(
+                'success' => false,
+                'message' => 'Error: Faltan campos requeridos en la respuesta del servidor'
+            );
+        }
+    } else {
+        $response = array(
+            'success' => false,
+            'message' => 'Usuario no encontrado o contraseña incorrecta'
+        );
+    }
+
+    $sentencia->close();
 } else {
     $response = array(
-        'success' => false, // Indica que el inicio de sesión falló
-        'message' => 'Usuario no encontrado o contraseña incorrecta' // Mensaje de error
+        'success' => false,
+        'message' => 'No se proporcionaron datos de inicio de sesión'
     );
 }
 
-    header('Content-Type: application/json');
-    echo json_encode($response, JSON_UNESCAPED_UNICODE);
-
-$sentencia->close();
 $mysql->close();
+
+header('Content-Type: application/json');
+echo json_encode($response, JSON_UNESCAPED_UNICODE);
 ?>
