4766e91516e2e65d60c24fd0136856d61d92c1d9
gmabs <mabel.garcia13402@est.uca.edu.ni>
2023-05-31 22:06:49 -0600

    agregar producto

--- app/src/main/java/com/example/matchastock/Entities/Product.kt
+++ app/src/main/java/com/example/matchastock/Entities/Product.kt
@@ -5,10 +5,10 @@ import com.google.gson.annotations.SerializedName
 data class Product(
     @SerializedName("idItem") val idItem: Int?,
     @SerializedName("nombreProd") val nombreProd: String,
-    @SerializedName("cantidadProd") val cantidadProd: Int?,
-    @SerializedName("estado") val estado: Int,
     @SerializedName("descripcionProd") val descripcionProd: String,
+    @SerializedName("cantidadProd") val cantidadProd: Int?,
     @SerializedName("imagen") val imagen : ByteArray?,
+    @SerializedName("estado") val estado: Int,
     @SerializedName("idUser") val idUser: Int?
 
 
