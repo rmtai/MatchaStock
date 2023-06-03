9ba41b03ab75073f8c488e389d270c146f7eb363
ashly31 <ashly.castillo13913@est.uca.edu.ni>
2023-06-03 02:33:38 -0600

    Detalles en el eliminar y editar para usuarios

--- MatchaStock/Usuario/editar.php
+++ MatchaStock/Usuario/editar.php
@@ -1,46 +1,30 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     require_once 'conexion.php';
+    $idUser = $_POST["idUser"];
     $nombre = $_POST["nombre"];
     $apellido = $_POST["apellido"];
     $username = $_POST["username"];
-    $passwordUser = $_POST["passwordUser"];
     $email = $_POST["email"];
-	
-	$hostname = "localhost";
-    $dbusername = "root";
-    $dbpassword = "";
-    $database = "matchastockdb";
-	
-	$conn = new mysqli($hostname, $dbusername, $dbpassword, $database);
+    $passwordUser = $_POST["passwordUser"];
 
-    // Verificar si se estableció la conexión correctamente
-if ($conn->connect_error) {
-    die("Error de conexión: " . $conn->connect_error);
-}
+    $query =
+    "UPDATE usuario SET 
+    nombre = '$nombre', 
+    apellido = '$apellido', 
+    username = '$username',
+    email = '$email',
+    passwordUser = '$passwordUser'
+    WHERE idUser = $idUser";
 
-// Verificar si se envió el ID del usuario
-if (isset($_POST["idUser"])) {
-    $idUser = $_POST["idUser"];
-    // Realizar la edición del usuario con el ID proporcionado
-    $sql = "UPDATE usuario SET nombre='$nombre', apellido='$apellido', username='$username', password='$passwordUser', email='$email' WHERE id=$idUser";
+    $result = $mysql->query($query);
 
-    if ($conn->query($sql) === TRUE) {
-        echo "Usuario editado correctamente";
+    if ($result == true) {
+        echo "Usuario editado satisfactoriamente...";
     } else {
-        echo "Error al editar el usuario: " . $conn->error;
+        echo "Error al editar usuario...";
     }
 } else {
-    // El ID del usuario no se proporcionó, realizar una inserción en su lugar
-    $sql = "INSERT INTO usuario (nombre, apellido, username, password, email) VALUES ('$nombre', '$apellido', '$username', '$passwordUser', '$email')";
-
-    if ($conn->query($sql) === TRUE) {
-        echo "Usuario creado correctamente";
-    } else {
-        echo "Error al crear el usuario: " . $conn->error;
-    }
-}
-
-// Cerrar la conexión a la base de datos
-$conn->close();
+    echo "Error desconocido";
+} 
 ?>
--- MatchaStock/Usuario/eliminar.php
+++ MatchaStock/Usuario/eliminar.php
@@ -1,18 +1,20 @@
 <?php
-    
     if($_SERVER["REQUEST_METHOD"] == "POST"){
         require_once 'conexion.php';
         $idUser = $_POST["idUser"];
-        $my_query = "delete from usuario where idUser =".$idUser;
-        $result = $mysql->query($my_query);
 
-        if($result == true){
+        $stmt = $mysql->prepare("DELETE FROM usuario WHERE idUser = ?");
+        $stmt->bind_param("i", $idUser);
+
+        if($stmt->execute()){
             echo 'Registro Eliminado con Exito';
-        } else { 
-            echo 'error';
+        } else {
+            echo 'Error al eliminar el registro';
         }
-    } else {
-        echo 'unknown error';
-    }
 
+        $stmt->close();
+        $mysql->close();
+    } else {
+        echo 'Unknown error';
+    }
 ?>
